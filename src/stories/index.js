535c88bac1af0b204c52e6f39d872a8446ed22b3
Nathan <terakilobyte@gmail.com>
2018-01-07 14:23:56 -0500

    adding storybook

---
+++ src/stories/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+import { storiesOf } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
+import { linkTo } from "@storybook/addon-links"
+
+import { Button, Welcome } from "@storybook/react/demo"
+
+import App from "../App"
+
+storiesOf("Default View", module).add("stuff", () => <App />)
+
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
+))
+
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
+  ))
+  .add("for funsies", () => (
+    <Button onClick={action("pressed")}>Pressed button</Button>
+  ))
